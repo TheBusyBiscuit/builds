4e24836824f4ac3c99fe83ba317deb78281271b7
TheBusyBiscuit <mrCookieSlime@gmail.com>
2017-12-17 18:04:51 +0100

    Release

---
+++ backend.js
@@ -0,0 +1,377 @@
+const https = require('https');
+const FileSystem = require('fs');
+const child_process = require('child_process');
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const header = {
+    "Accept": "application/vnd.github.v3+json",
+    "User-Agent": "The Busy Biscuit's Repository Compiler",
+    "Time-Zone": "UTC"
+}
+
+FileSystem.unlinkSync("app.log");
+
+var log = console.log;
+
+console.log = function(msg) {
+    log(msg);
+    FileSystem.appendFile("app.log", msg + "\n", function(err) {
+        if (err) {
+            log(err);
+        }
+    });
+}
+
+console.log("Watching Repositories...");
+
+FileSystem.readFile('repos.json', 'UTF-8', function(err, data) {
+    if (!err) {
+        var repos = JSON.parse(data);
+
+        for (var author in repos) {
+            console.log(" Watching Author \"" + author + "\"...");
+            for (var i in repos[author]) {
+                var repo = repos[author][i];
+                var repository = repo.split(':')[0];
+                var branch = repo.split(':')[1];
+
+                console.log("  Watching Repository \"" + author + "/" + repository + "\" on Branch \"" + branch + "\"...");
+
+                loadLatestCommit(author, repository, branch);
+            }
+        }
+    }
+    else {
+        console.log(error);
+    }
+});
+
+function loadLatestCommit(author, repo, branch) {
+    var options = {
+        host: "api.github.com",
+        path: "/repos/" + author +"/" + repo + "/commits?per_page=1&sha=" + branch,
+        headers: header
+    }
+
+    https.get(options, function(response) {
+        console.log(response.statusCode + " - " + response.statusMessage);
+        if (response.statusCode == 200) {
+            var body = '';
+
+            response.on('data', function(data) {
+                body += data;
+            });
+
+            response.on('end', function() {
+                var json = JSON.parse(body);
+
+                if (!json.documentation_url) {
+                    getLicense(author, repo, branch, json[0]);
+                }
+                else {
+                    console.log(author + "/" + repo + ": " + json.message);
+                }
+            });
+        }
+    }).on('error', function(err) {
+        console.log(err);
+    });
+}
+
+function getLicense(author, repo, branch, commit) {
+    var options = {
+        host: "api.github.com",
+        path: "/repos/" + author +"/" + repo + "/license",
+        headers: header
+    }
+    https.get(options, function(response) {
+        console.log(response.statusCode + " - " + response.statusMessage);
+        if (response.statusCode == 200) {
+            var body = '';
+
+            response.on('data', function(data) {
+                body += data;
+            });
+
+            response.on('end', function() {
+                var json = JSON.parse(body);
+
+                if (!json.documentation_url && json.license.url != null) {
+                    commit.license = {
+                        name: json.license.name,
+                        id: json.license.spdx_id,
+                        url: json.download_url
+                    };
+                }
+                else {
+                    commit.license = {
+                        name: "",
+                        id: "",
+                        url: ""
+                    }
+                }
+
+                watchRepository(author, repo, branch, commit);
+            });
+        }
+    }).on('error', function(err) {
+        console.log(err);
+    });
+}
+
+function watchRepository(author, repo, branch, commit) {
+    if (!FileSystem.existsSync(author)) {
+        FileSystem.mkdirSync(author);
+    }
+    if (!FileSystem.existsSync(author + "/" + repo)) {
+        FileSystem.mkdirSync(author + "/" + repo);
+    }
+    if (!FileSystem.existsSync(author + "/" + repo + "/" + branch)) {
+        FileSystem.mkdirSync(author + "/" + repo + "/" + branch);
+    }
+    if (!FileSystem.existsSync(author + "/" + repo + "/" + branch + "/files")) {
+        FileSystem.mkdirSync(author + "/" + repo + "/" + branch + "/files");
+    }
+
+    if (FileSystem.existsSync(author + "/" + repo + "/" + branch + "/builds.json")) {
+        FileSystem.readFile(author + "/" + repo + "/" + branch + "/builds.json", 'UTF-8', function(err, data) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                compareBuilds(author, repo, branch, JSON.parse(data), commit);
+            }
+        });
+    }
+    else {
+        compareBuilds(author, repo, branch, {}, commit);
+    }
+}
+
+function compareBuilds(author, repo, branch, builds, commit) {
+    var date = "";
+
+    date += commit.commit.committer.date.split("T")[0].split("-")[2] + " ";
+    date += months[parseInt(commit.commit.committer.date.split("T")[0].split("-")[1]) - 1] + " ";
+    date += commit.commit.committer.date.split("T")[0].split("-")[0] + " (";
+    date += commit.commit.committer.date.split("T")[1].replace("Z", "") + ")";
+
+    var data = {
+        id: 1,
+        sha: commit.sha,
+        date: date,
+        timestamp: parseInt(commit.commit.committer.date.replace(/\D/g, "")),
+        message: commit.commit.message,
+        author: commit.author.login,
+        avatar: commit.author.avatar_url,
+        license: commit.license,
+        status: "PENDING"
+    }
+
+    if (builds.latest && builds[builds.latest].timestamp < data.timestamp) {
+        data.id = builds.latest + 1;
+    }
+
+    if (!builds.latest || (builds.latest && builds[builds.latest].timestamp < data.timestamp)) {
+        builds.latest = data.id;
+
+        builds[data.id] = data;
+
+        FileSystem.writeFile(author + "/" + repo + "/" + branch + "/builds.json", JSON.stringify(builds, null, 4), 'UTF-8');
+
+        clone(author, repo, branch, commit.sha, builds, data);
+        generateHTML(author, repo, branch, builds);
+    }
+}
+
+function clone(author, repo, branch, commit, builds, data) {
+    console.log("Cloning Repository \"" + author + "/" + repo + "\"...");
+    var clone = child_process.spawn("git", ["clone", "https://github.com/" + author + "/" + repo + ".git", author + "/" + repo + "/" + branch + "/files", "-b", branch, "--single-branch"]);
+
+    clone.stderr.on('data', function(data) {
+        console.log(" " + data);
+    });
+
+    clone.stdout.on('data', function(data) {
+        console.log(" " + data);
+    });
+
+    clone.on('close', function(status) {
+        var reset = child_process.spawn("git", ["reset", "--hard", commit], {cwd: author + "/" + repo + "/" + branch + "/files"});
+
+        reset.on('close', function() {
+            pom(author, repo, branch, builds, data.id);
+        });
+    });
+}
+
+function pom(author, repo, branch, builds, id) {
+    FileSystem.readFile(author + "/" + repo + "/" + branch + "/files/pom.xml", 'UTF-8', function(err, data) {
+        if (!err) {
+            data = data.replace(/\r?\n|\r/g, "");
+            var build = data.match(/<build>.*<\/build>/)[0];
+
+            var important = "";
+            important += pomFilter(build, /<resources>.*<\/resources>/);
+            important += pomFilter(build, /<testResources>.*<\/testResources>/);
+            important += pomFilter(build, /<sourceDirectory>.*<\/sourceDirectory>/);
+            important += pomFilter(build, /<testSourceDirectory>.*<\/testSourceDirectory>/);
+
+            function pomFilter(build, regex) {
+                var match = build.match(regex);
+
+                if (match) return match[0];
+                else return "";
+            }
+
+            data = data.replace(/<build>.*<\/build>/, "<build><finalName>" + repo + "-" + id + "</finalName>" + important + "</build>");
+
+            FileSystem.writeFile(author + "/" + repo + "/" + branch + "/files/pom.xml", data, 'UTF-8', function(err) {
+                if (!err) {
+                    compile(author, repo, branch, builds, id);
+                }
+                else {
+                    console.log(err);
+                }
+            });
+        }
+        else {
+            console.log(err);
+        }
+    });
+}
+
+function compile(author, repo, branch, builds, id) {
+    console.log("Compiling Repository \"" + author + "/" + repo + "\"...");
+    var maven = child_process.spawn("mvn", ["package"], {cwd: __dirname + "/" + author + "/" + repo + "/" + branch + "/files", shell: true});
+
+    maven.stderr.on('data', function(data) {
+        console.log(" " + data);
+
+        FileSystem.appendFile(author + "/" + repo + "/" + branch + "/" + repo + "-" + id + ".log", data + "\n", function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+
+    maven.stdout.on('data', function(data) {
+        console.log(" " + data);
+
+        FileSystem.appendFile(author + "/" + repo + "/" + branch + "/" + repo + "-" + id + ".log", data + "\n", function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+
+    maven.on('close', function(status) {
+        if (status == 0) {
+            builds[id].status = "SUCCESS";
+            builds.last_successful = id;
+
+            FileSystem.writeFile(author + "/" + repo + "/" + branch + "/builds.json", JSON.stringify(builds, null, 4), 'UTF-8', function(err) {
+                if (!err) {
+                    FileSystem.rename(author + "/" + repo + "/" + branch + "/files/target/" + repo + "-" + id + ".jar", author + "/" + repo + "/" + branch + "/" + repo + "-" + id + ".jar", function(err) {
+                        if (!err) {
+                            clearFolder(author + "/" + repo + "/" + branch + "/files", function(err) {
+                                if (err) {
+                                    // FINISHED
+                                    console.log(err);
+                                }
+                            });
+                        }
+                        else {
+                            console.log(err);
+                        }
+                    });
+                }
+                else {
+                    console.log(err);
+                }
+            });
+        }
+        else {
+            builds[id].status = "FAILURE";
+            FileSystem.writeFile(author + "/" + repo + "/" + branch + "/builds.json", JSON.stringify(builds, null, 4), 'UTF-8');
+        }
+    });
+}
+
+function generateHTML(author, repo, branch, builds) {
+    FileSystem.readFile("template.html", 'UTF-8', function(err, data) {
+        if (!err) {
+            data = data.replace(/\${owner}/g, author);
+            data = data.replace(/\${repository}/g, repo);
+            data = data.replace(/\${branch}/g, branch);
+            data = data.replace(/\${builds}/g, builds.latest);
+
+            FileSystem.writeFile(author + "/" + repo + "/" + branch + "/index.html", data, 'UTF-8');
+        }
+        else {
+            console.log(err);
+        }
+    });
+}
+
+function clearFolder(path, callback) {
+    FileSystem.stat(path, function(err, stats) {
+        if(err) {
+            callback(err);
+            return;
+        }
+
+        if(stats.isFile()) {
+            FileSystem.unlink(path, function(err) {
+                if(err) {
+                    callback(err);
+                }
+                else{
+                    callback(null);
+                }
+            });
+        }
+        else if(stats.isDirectory()) {
+            FileSystem.readdir(path, function(err, files) {
+                if(err) {
+                    callback(err);
+                    return;
+                }
+
+                var length = files.length;
+                var index = 0;
+
+                function check() {
+                    if(length === index) {
+                        FileSystem.rmdir(path, function(err) {
+                            if(err) {
+                                callback(err);
+                            }
+                            else{
+                                callback(null);
+                            }
+                        });
+                        return true;
+                    }
+                    return false;
+                };
+
+                if(!check()) {
+                    for (var i = 0; i < length; i++) {
+                        clearFolder(path + '/' + files[i], function(err, status) {
+                            if(!err) {
+                                index++;
+                                check();
+                            }
+                            else {
+                                callback(err);
+                                return;
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    });
+  }
---
+++ frontend.js
@@ -0,0 +1,119 @@
+$(function() {
+    var body = $(document.body);
+    var owner = body.attr("owner");
+    var repository = body.attr("repository");
+    var branch = body.attr("branch");
+
+    $.getJSON("https://thebusybiscuit.github.io/builds/" + owner + "/" + repository + "/" + branch + "/builds.json", function(builds) {
+        var last_successful = builds.last_successful;
+
+        // Get currently selected Build
+        var current = builds.latest;
+
+        if(window.location.hash) {
+            var hash = window.location.hash.substr(1);
+
+            if (!isNaN(hash)) {
+                var id = parseInt(hash);
+                if (id > 0 && id < builds.latest) {
+                    current = id;
+                }
+            }
+        }
+
+        // Load currently selected Build
+        loadBuild(builds, current);
+
+        // "Last Successful Build" Link
+        var link_last_successful = $("#link_last_successful_build");
+
+        link_last_successful.text("#" + last_successful);
+        link_last_successful.attr("href", "#" + last_successful);
+
+        // Build List
+        var list_builds = $("#buildlist");
+
+        for (var i = builds.latest; i > 0; i--) {
+            list_builds.append(build(builds, i));
+        }
+
+        // Add Click Events
+        $(".trigger").click(function() {
+            loadBuild(builds, parseInt($(this).attr("href").substr(1)));
+        })
+    });
+
+    function build(builds, id) {
+        var stroke = "rgb(110, 110, 110)";
+        var color = "rgb(160, 160, 160)";
+
+        if (builds[id].status === "SUCCESS") {
+            stroke = "rgb(60, 100, 60)";
+            color = "rgb(20, 255, 20)";
+        }
+        else if (builds[id].status === "FAILURE") {
+            stroke = "rgb(100, 60, 60)";
+            color = "rgb(255, 20, 20)";
+        }
+
+        var html = "<div class=\"build\" id=\"" + id + "\">";
+        html += "<svg class=\"build_child build_state\">";
+        html += "<circle cx=\"12\" cy=\"29\" r=\"11\" stroke=\"" + stroke + "\" stroke-width=\"2\" fill=\"" + color + "\"/></svg>";
+        html += "<a class=\"trigger build_child link_build\" href=\"#" + id + "\">#" + id + "</a>";
+        html += "<a class=\"trigger build_child link_date\" href=\"#" + id + "\">" + builds[id].date + "</a>";
+        html += "<a class=\"build_child link_commit\" href=https://github.com/" + owner + "/" + repository + "/commit/" + builds[id].sha + ">#" + builds[id].sha.substr(0, 5) + "</a>";
+        html += "</div>";
+
+        return html;
+    }
+
+    function loadBuild(builds, id) {
+        var stroke = "rgb(110, 110, 110)";
+        var color = "rgb(160, 160, 160)";
+
+        if (builds[id].status === "SUCCESS") {
+            stroke = "rgb(60, 100, 60)";
+            color = "rgb(20, 255, 20)";
+        }
+        else if (builds[id].status === "FAILURE") {
+            stroke = "rgb(100, 60, 60)";
+            color = "rgb(255, 20, 20)";
+        }
+
+        var current_icon = "<circle cx=\"31\" cy=\"31\" r=\"23\" stroke=\"" + stroke + "\" stroke-width=\"2\" fill=\"" + color + "\"/>";
+
+        $("#current_icon").html(current_icon);
+        $("#current_status").text(builds[id].status);
+        $("#current_name").text(repository + " - #" + id);
+
+        var download_jar = $("#current_download_jar");
+        download_jar.text(repository + "-" + id + ".jar");
+        download_jar.attr("href", repository + "-" + id + ".jar");
+
+        var download_log = $("#current_download_log");
+        download_log.text(repository + "-" + id + ".log");
+        download_log.attr("href", repository + "-" + id + ".log");
+
+        $("#current_tree").attr("href", "https://github.com/" + owner + "/" + repository + "/tree/" + builds[id].sha);
+
+        if (builds[id].license === "") {
+            $("#license_section").css("display", "none");
+        }
+        else {
+            $("#license_section").css("display", "");
+
+            var current_license = $("#current_license");
+            current_license.attr("href", builds[id].license.url);
+            current_license.text(builds[id].license.name);
+        }
+
+        var current_commit = $("#current_commit");
+        current_commit.attr("href", "https://github.com/" + owner +"/" + repository + "/commit/" + builds[id].sha);
+        current_commit.text("#" + builds[id].sha.substr(0, 10));
+
+        $("#current_commit_avatar").attr("src", builds[id].avatar);
+        $("#current_commit_committer").text(builds[id].author);
+        $("#current_commit_date").text(builds[id].date);
+        $("#current_commit_message").text("\"" + builds[id].message + "\"");
+    }
+});
