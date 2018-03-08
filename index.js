91f6ee2d164d0e457508bbcb259e3eb079119f4b
TheBusyBiscuit <mrCookieSlime@gmail.com>
2018-03-08 13:02:08 +0100

    Fixed index spacing

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ function addRepository(owner, repo, branch) {
     html += owner + "/" + repo + " (" + branch + ")</a>";
     html += "<table class=\"info_table\">";
     html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/briefcase.svg\"></td>";
-    html += "<td>Builds</td><td>";
+    html += "<td class=\"table_label\">Builds</td><td>";
     html += "<a class=\"link_info\" href=\"" + owner + "/" + repo + "/" + branch + "\">Repo Server</a></td></tr>";
     html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/repo.svg\"></td>";
-    html += "<td>Repository</td><td>";
+    html += "<td class=\"table_label\">Repository</td><td>";
     html += "<a class=\"link_info\" href=\"https://github.com/" + owner + "/" + repo + "/tree/" + branch + "\">GitHub</a></td></tr>";
     html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/calendar.svg\"></td>";
-    html += "<td>Latest Build</td><td>";
+    html += "<td class=\"table_label\">Latest Build</td><td>";
     html += "<a id=\"latest_" + owner + "_" + repo + "_" + branch + "\" class=\"link_info\" href=\"${latest}\">";
     html += "<img style=\"width: 20px;\" class=\"loading\" src=\"https://thebusybiscuit.github.io/content/octicons/sync.svg\"/></a></td></tr>";
     html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/beaker.svg\"></td>";
-    html += "<td>Status</td><td>";
+    html += "<td class=\"table_label\">Status</td><td>";
     html += "<img src=\"https://thebusybiscuit.github.io/builds/" + owner + "/" + repo + "/" + branch + "/badge.svg\"/></td></tr>";
     html += "</table></div>";
 
@@ -37,6 +37,6 @@ function addRepository(owner, repo, branch) {
         let latest = $("#latest_" + owner + "_" + repo + "_" + branch);
 
         latest.attr("href", owner + "/" + repo + "/" + branch + "#" + builds.latest);
-        latest.text("#" + (builds[builds.latest].tag ? builds[builds.latest].tag: builds.latest) + " - " + builds[builds.latest].date);
+        latest.text((builds[builds.latest].tag ? builds[builds.latest].tag: ("#" + builds.latest)) + " - " + builds[builds.latest].date);
     });
 }
