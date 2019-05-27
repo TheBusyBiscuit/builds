415dcc8ce9022a10987d5ca67aa19e22c5791751
TheBusyBiscuit <TheBusyBiscuit@users.noreply.github.com>
2019-05-27 20:20:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,20 @@ function addRepository(owner, repo, branch) {
     html += "<a class=\"link_build link_repo\" href=\"" + owner + "/" + repo + "/" + branch + "\">";
     html += owner + "/" + repo + " (" + branch + ")</a>";
     html += "<table class=\"info_table\">";
-    html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/briefcase.svg\"></td>";
-    html += "<td class=\"table_label\">Builds</td><td>";
-    html += "<a class=\"link_info\" href=\"" + owner + "/" + repo + "/" + branch + "\">Repo Server</a></td></tr>";
-    html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/repo.svg\"></td>";
-    html += "<td class=\"table_label\">Repository</td><td>";
-    html += "<a class=\"link_info\" href=\"https://github.com/" + owner + "/" + repo + "/tree/" + branch + "\">GitHub</a></td></tr>";
+    
+//    html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/briefcase.svg\"></td>";
+//    html += "<td class=\"table_label\">Builds</td><td>";
+//    html += "<a class=\"link_info\" href=\"" + owner + "/" + repo + "/" + branch + "\">Repo Server</a></td></tr>";
+    
+//    html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/repo.svg\"></td>";
+//    html += "<td class=\"table_label\">Repository</td><td>";
+//    html += "<a class=\"link_info\" href=\"https://github.com/" + owner + "/" + repo + "/tree/" + branch + "\">GitHub</a></td></tr>";
+    
     html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/calendar.svg\"></td>";
     html += "<td class=\"table_label\">Latest Build</td><td>";
     html += "<a id=\"latest_" + owner + "_" + repo + "_" + branch + "\" class=\"link_info\" href=\"${latest}\">";
     html += "<img style=\"width: 20px;\" class=\"loading\" src=\"https://thebusybiscuit.github.io/content/octicons/sync.svg\"/></a></td></tr>";
+    
     html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/beaker.svg\"></td>";
     html += "<td class=\"table_label\">Status</td><td>";
     html += "<img src=\"https://thebusybiscuit.github.io/builds/" + owner + "/" + repo + "/" + branch + "/badge.svg\"/></td></tr>";
