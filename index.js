2995082502f1c8667cd20d3690c1777a27e7efd9
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-06-16 11:48:17 +0200

    Changed main page design

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
 $(function() {
     $.getJSON("https://thebusybiscuit.github.io/builds/repos.json", function(repos) {
         $("#repos").html("");
+        var profiles = [];
 
         for (var repo in repos) {
-            console.log("Found Project \"" + repo + "\"");
-            addRepository(repo.split("/")[0], repo.split('/')[1].split(":")[0], repo.split('/')[1].split(":")[1]);
+            if (!profiles.includes(repo.split("/")[0])) {
+                console.log("Found Author \"" + repo.split("/")[0] + "\"");
+                profiles.push(repo.split("/")[0]);
+                addProfile(repo.split("/")[0], repos);
+            }
         }
+
+        console.log(profiles);
     });
 });
 
-function addRepository(owner, repo, branch) {
-    let html = "<div class=\"box repo\">";
-    html += "<a class=\"link_build link_repo\" href=\"" + owner + "/" + repo + "/" + branch + "\">";
-    html += owner + "/" + repo + " (" + branch + ")</a>";
-    html += "<table class=\"info_table\">";
-    
-//    html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/briefcase.svg\"></td>";
-//    html += "<td class=\"table_label\">Builds</td><td>";
-//    html += "<a class=\"link_info\" href=\"" + owner + "/" + repo + "/" + branch + "\">Repo Server</a></td></tr>";
-    
-//    html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/repo.svg\"></td>";
-//    html += "<td class=\"table_label\">Repository</td><td>";
-//    html += "<a class=\"link_info\" href=\"https://github.com/" + owner + "/" + repo + "/tree/" + branch + "\">GitHub</a></td></tr>";
-    
-    html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/calendar.svg\"></td>";
-    html += "<td class=\"table_label\">Latest Build</td><td>";
-    html += "<a id=\"latest_" + owner + "_" + repo + "_" + branch + "\" class=\"link_info\" href=\"${latest}\">";
-    html += "<img style=\"width: 20px;\" class=\"loading\" src=\"https://thebusybiscuit.github.io/content/octicons/sync.svg\"/></a></td></tr>";
-    
+function addProfile(user, repos) {
+    $("#repos").append('<div class="box" style="width: 50%; margin: 2vh auto;"><a style="padding-top: 1vh;" class="link_build link_repo" href="https://github.com/' + user + '">' + user + '</a><table id="projects_' + user + '" class="info_table"></table></div>');
+    var table = $("#projects_" + user);
+
+    for (var repo in repos) {
+        if (repo.split("/")[0] !== user) continue;
+
+        console.log("Found Project \"" + repo + "\"");
+        addRepository(table, user, repo.split('/')[1].split(":")[0], repo.split('/')[1].split(":")[1]);
+    }
+}
+
+function addRepository(table, owner, repo, branch) {
+    let html = "";
+
     html += "<tr><td class=\"icon\"><img class=\"icon\" src=\"https://thebusybiscuit.github.io/content/octicons/beaker.svg\"></td>";
-    html += "<td class=\"table_label\">Status</td><td>";
-    html += "<img src=\"https://thebusybiscuit.github.io/builds/" + owner + "/" + repo + "/" + branch + "/badge.svg\"/></td></tr>";
-    html += "</table></div>";
+    html += '<td class="table_label" style="width: auto;"><a class="link_info" href="' + owner + '/' + repo + '/' + branch + '">' + (owner + "/" + repo + " (" + branch + ")") + "</a></td><td>";
+    html += '<img style="float: right;" src="https://thebusybiscuit.github.io/builds/' + owner + '/' + repo + '/' + branch + '/badge.svg"/></td></tr>';
 
-    $("#repos").append(html);
-
-    $.getJSON("https://thebusybiscuit.github.io/builds/" + owner + "/" + repo + "/" + branch + "/builds.json", function(builds) {
-        let latest = $("#latest_" + owner + "_" + repo + "_" + branch);
-
-        latest.attr("href", owner + "/" + repo + "/" + branch + "#" + builds.latest);
-        latest.text((builds[builds.latest].candidate === "RELEASE" ? builds[builds.latest].tag: ("#" + builds.latest)) + " - " + builds[builds.latest].date);
-    });
+    table.append(html);
 }
