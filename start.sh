bf8c6c2c3d2c3a9567c2c18efe53c94043ab87a3
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-06-23 13:15:36 +0200

    Added more mocha & sonar integrations

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Pull the latest Version from GitHub
+git pull
+
+# Perform tests + code coverage
+npm run test
+npm run code-coverage
+
+# Report results to sonarcloud.io
+npm run sonar
+
+# Run program
+npm run remote
