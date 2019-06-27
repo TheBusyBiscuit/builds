0e2cfd8ee65915120b3fea58834cd06beb5119f5
TheBusyBiscuit <abusybiscuit@gmail.com>
2019-06-27 12:41:40 +0200

    Added logger.js test

--- test/TestEnvironment.js
+++ test/TestEnvironment.js
@@ -2,7 +2,9 @@ const process = require('child-process-promise');
 
 const chai = require('chai');
 chai.use(require('chai-as-promised'));
-const {assert} = chai;
+const {assert, expect} = chai;
+
+const logger = require('../src/logger.js');
 
 describe("Environment Test", function() {
     this.timeout(6000);
@@ -21,5 +23,23 @@ describe("Environment Test", function() {
 
     it("has Maven installed", () => {
         return assert.isFulfilled(process.exec("mvn -v"));
+    });
+
+    it("has a working logger.js script (logging = false)", () => {
+        return expect(() => {
+            logger(false, "Hello World");
+        }).to.not.throw();
+    });
+
+    it("has a working logger.js script (logging = true)", () => {
+        return expect(() => {
+            var log = console.log;
+            // Temporarily disable console.log
+            console.log = (str) => {};
+
+            logger(true, "Hello World");
+
+            console.log = log;
+        }).to.not.throw();
     });
 });
