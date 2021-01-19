5ead56f5b433f2e2dd6b6f6e304b01aafd65f6e7
TheBusyBiscuit <mrCookieSlime@gmail.com>
2021-01-19 16:59:56 +0100

    Lots of improvements + dark mode

--- src/frontend.js
+++ src/frontend.js
@@ -1,13 +1,14 @@
-$(function() {
-    var body = $(document.body);
-    var owner = body.attr("owner");
-    var repository = body.attr("repository");
-    var branch = body.attr("branch");
+$(() => {
+    let body = $(document.body);
+    let owner = body.attr("owner");
+    let repository = body.attr("repository");
+    let branch = body.attr("branch");
 
-    $.getJSON("https://thebusybiscuit.github.io/builds/resources/repos.json", function(repos) {
-        var info = repos[owner + "/" + repository + ":" + branch];
-        var directory = owner + "/" + repository + "/" + branch;
+    $.getJSON("https://thebusybiscuit.github.io/builds/resources/repos.json", repos => {
+        let info = repos[`${owner}/${repository}:${branch}`];
+        let directory = `${owner}/${repository}/${branch}`;
 
+        // Override via custom directory option
         if (info.options && info.options.custom_directory) {
             directory = info.options.custom_directory;
         }
@@ -15,9 +16,9 @@ $(function() {
         createBadge(directory, "markdown");
         createBadge(directory, "html");
 
-	    let i = 1;
-        for (var key in info.dependencies) {
-            $("#dependencies").append('<tr id="custom-info-' + i++ + '">' + key + '</td>');
+        let i = 1;
+        for (let key in info.dependencies) {
+            $("#dependencies").append(`<tr id="custom-info-${i++}">${key}</td>`);
         }
 
         if (i > 1) {
@@ -25,117 +26,120 @@ $(function() {
         }
 
         function selectBuild(builds, id) {
-            var stroke = "rgb(110, 110, 110)";
-            var color = "rgb(160, 160, 160)";
+            let stroke = "rgb(110, 110, 110)";
+            let color = "rgb(160, 160, 160)";
 
             if (builds[id].status === "SUCCESS") {
                 stroke = "rgb(60, 100, 60)";
                 color = "rgb(20, 255, 20)";
-            }
-            else if (builds[id].status === "FAILURE") {
+            } else if (builds[id].status === "FAILURE") {
                 stroke = "rgb(100, 60, 60)";
                 color = "rgb(255, 20, 20)";
             }
 
-            var current_icon = "<circle cx=\"31\" cy=\"31\" r=\"23\" stroke=\"" + stroke + "\" stroke-width=\"2\" fill=\"" + color + "\"/>";
+            let current_icon = `<circle cx="31" cy="31" r="23" stroke="${stroke}" stroke-width="2" fill="${color}"/>`;
 
             $("#current_icon").html(current_icon);
             $("#current_status").text(builds[id].status);
 
-            var download_jar = $("#current_download_jar");
+            let download_jar = $("#current_download_jar");
 
             if (builds[id].status === "SUCCESS") {
                 $("#download_section").css("display", "");
-                download_jar.attr("href", repository + "-" + id + ".jar");
-            }
-            else {
+                download_jar.attr("href", `${repository}-${id}.jar`);
+            } else {
                 $("#download_section").css("display", "none");
             }
 
             if (builds[id].candidate === "RELEASE") {
-                download_jar.text(repository + " " + builds[id].tag + ".jar");
-                download_jar.attr("download", repository + " " + builds[id].tag + ".jar");
+                download_jar.text(`${repository} ${builds[id].tag}.jar`);
+                download_jar.attr("download", `${repository} ${builds[id].tag}.jar`);
 
-                $("#current_name").text(repository + " " + builds[id].tag);
+                $("#current_name").text(`${repository} ${builds[id].tag}`);
                 $("#tag_section").css("display", "");
 
-                var current_tag = $("#current_tag");
-                current_tag.attr("href", "https://github.com/" + owner +"/" + repository + "/releases/tag/" + builds[id].tag);
+                let current_tag = $("#current_tag");
+                current_tag.attr("href", `https://github.com/${owner}/${repository}/releases/tag/${builds[id].tag}`);
                 current_tag.text(builds[id].tag);
-            }
-            else {
-                var prefix = info.options ? info.options.prefix: "DEV";
-                download_jar.text(repository + " - " + prefix + " " + id + " (git " + builds[id].sha.substr(0, 5) + ").jar");
-                download_jar.attr("download", repository + " - " + prefix + " " + id + " (git " + builds[id].sha.substr(0, 5) + ").jar");
+            } else {
+                let prefix = info.options ? info.options.prefix : "DEV";
+                download_jar.text(`${repository} - ${prefix} ${id} (git ${builds[id].sha.substr(0, 5)}).jar`);
+                download_jar.attr("download", `${repository} - ${prefix} ${id} (git ${builds[id].sha.substr(0, 5)}).jar`);
 
-                $("#current_name").text(repository + " - #" + id);
+                $("#current_name").text(`${repository} - #${id}`);
                 $("#tag_section").css("display", "none");
             }
 
-            var download_log = $("#current_download_log");
-            download_log.text(repository + "-" + id + ".log");
-            download_log.attr("href", repository + "-" + id + ".log");
+            let download_log = $("#current_download_log");
+            download_log.text(`${repository}-${id}.log`);
+            download_log.attr("href", `${repository}-${id}.log`);
 
-            $("#current_tree").attr("href", "https://github.com/" + owner + "/" + repository + "/tree/" + builds[id].sha);
+            $("#current_tree").attr("href", `https://github.com/${owner}/${repository}/tree/${builds[id].sha}`);
 
             if (builds[id].license === "") {
                 $("#license_section").css("display", "none");
-            }
-            else {
+            } else {
                 $("#license_section").css("display", "");
 
-                var current_license = $("#current_license");
+                let current_license = $("#current_license");
                 current_license.attr("href", builds[id].license.url);
                 current_license.text(builds[id].license.name);
             }
 
-            var current_commit = $("#current_commit");
-            current_commit.attr("href", "https://github.com/" + owner +"/" + repository + "/commit/" + builds[id].sha);
+            let current_commit = $("#current_commit");
+            current_commit.attr("href", `https://github.com/${owner}/${repository}/commit/${builds[id].sha}`);
             current_commit.text("#" + builds[id].sha.substr(0, 5));
 
             $("#current_commit_avatar").attr("src", builds[id].avatar);
             $("#current_commit_committer").text(builds[id].author);
             $("#current_commit_date").text(builds[id].date);
 
-            var msg = "\"" + builds[id].message + "\"";
+            let msg = `"${builds[id].message}"`;
+
             // Prevent XSS
             msg = msg.replace(/</g, "")
             msg = msg.replace(/>/g, "")
 
-            var matches = msg.match(/#[0-9]+/g);
+            let matches = msg.match(/#[0-9]+/g);
 
             for (let n in matches) {
-                msg = msg.replace(matches[n], "<a class=\"link_info\" href=https://github.com/" + owner + "/" + repository + "/issues/" + matches[n].replace("#", "") + ">" + matches[n] + "</a>");
+                msg = msg.replace(matches[n], `<a class="link_info" href=https://github.com/"${owner}/${repository}/issues/${matches[n].replace("#", "")}>${matches[n]}</a>`);
             }
 
             $("#current_commit_message").html(msg);
 
-            var j = 1;
-            for (var label in info.dependencies) {
-                var content = "";
-                var prev = 0;
+            let j = 1;
+            for (let label in info.dependencies) {
+                let content = "";
+                let prev = 0;
 
-                for (var min in info.dependencies[label]) {
+                for (let min in info.dependencies[label]) {
                     if (id >= parseInt(min) && parseInt(min) > prev) {
                         content = info.dependencies[label][min];
                     }
                 }
 
-                $("#custom-info-" + j++).html('<td class="icon"><img class="icon" src="https://cdnjs.cloudflare.com/ajax/libs/octicons/8.5.0/svg/package.svg"></td><td class="info_table_middle">' + label + '</td><td>' + content + '</td>');
+                $("#custom-info-" + j++).html(`
+                    <td class="icon">
+                        <img class="icon" alt="package icon" src="https://cdnjs.cloudflare.com/ajax/libs/octicons/8.5.0/svg/package.svg">
+                    </td>
+                    <td class="info_table_middle">${label}</td><td>${content}</td>
+                `);
             }
         }
 
-        $.getJSON("https://thebusybiscuit.github.io/builds/" + directory + "/builds.json", function(builds) {
-            var last_successful = builds.last_successful;
+        $.getJSON(`https://thebusybiscuit.github.io/builds/${directory}/builds.json`, builds => {
+            let last_successful = builds.last_successful;
 
             // Get currently selected Build
-            var current = builds.latest;
+            let current = builds.latest;
 
-            if(window.location.hash) {
-                var hash = window.location.hash.substr(1);
+            if (window.location.hash) {
+                let hash = window.location.hash.substr(1);
 
                 if (!isNaN(hash)) {
-                    var id = parseInt(hash);
+                    let id = parseInt(hash);
+
                     if (id > 0 && id < builds.latest) {
                         current = id;
                     }
@@ -146,46 +150,45 @@ $(function() {
             selectBuild(builds, current);
 
             // "Last Successful Build" Link
-            var link_last_successful = $("#link_last_successful_build");
-
+            let link_last_successful = $("#link_last_successful_build");
             link_last_successful.text("#" + last_successful);
             link_last_successful.attr("href", "#" + last_successful);
 
             $(".build_header").text("Builds (" + builds.latest + ")");
 
             // Build List
-            var list_builds = $("#buildlist");
+            let list_builds = $("#buildlist");
 
             list_builds.html("");
-            for (var buildID = builds.latest; buildID > 0; buildID--) {
+
+            for (let buildID = builds.latest; buildID > 0; buildID--) {
                 list_builds.append(build(builds, buildID));
             }
 
             // Add Click Events
-            $(".trigger").click(function() {
+            $(".trigger").click(() => {
                 selectBuild(builds, parseInt($(this).attr("href").substr(1)));
             });
         });
     });
 
-    $("#badge_click").click(function() {
+    $("#badge_click").click(() => {
         $(".overlay").css("display", "");
     });
 
-    $("#close_badge").click(function() {
+    $("#close_badge").click(() => {
         $(".overlay").css("display", "none");
     });
 
     function build(builds, id) {
-        var stroke = "rgb(110, 110, 110)";
-        var color = "rgb(160, 160, 160)";
-        var name = "#" + id;
+        let stroke = "rgb(110, 110, 110)";
+        let color = "rgb(160, 160, 160)";
+        let name = "#" + id;
 
         if (builds[id].status === "SUCCESS") {
             stroke = "rgb(60, 100, 60)";
             color = "rgb(20, 255, 20)";
-        }
-        else if (builds[id].status === "FAILURE") {
+        } else if (builds[id].status === "FAILURE") {
             stroke = "rgb(100, 60, 60)";
             color = "rgb(255, 20, 20)";
         }
@@ -194,20 +197,16 @@ $(function() {
             name = builds[id].tag;
         }
 
-        var html = "<div class=\"build";
-
-        if (builds[id].candidate === "RELEASE") html += " release";
-        else html += " dev";
-
-        html += "\" id=\"build_" + id + "\">";
-        html += "<svg class=\"build_child build_state\">";
-        html += "<circle cx=\"12\" cy=\"29\" r=\"11\" stroke=\"" + stroke + "\" stroke-width=\"2\" fill=\"" + color + "\"/></svg>";
-        html += "<a class=\"trigger build_child link_build\" href=\"#" + id + "\">" + name + "</a>";
-        html += "<a class=\"trigger build_child link_date\" href=\"#" + id + "\">" + builds[id].date + "</a>";
-        html += "<a class=\"build_child link_commit\" href=https://github.com/" + owner + "/" + repository + "/commit/" + builds[id].sha + ">#" + builds[id].sha.substr(0, 5) + "</a>";
-        html += "</div>";
-
-        return html;
+        return `
+            <div class="build ${builds[id].candidate === "RELEASE" ? "release" : "dev"}" id="build_${id}">
+                <svg class="build_child build_state">
+                    <circle cx="12" cy="29" r="11" stroke="${stroke}" stroke-width="2" fill="${color}"/>
+                </svg>
+                <a class="trigger build_child link_build" href="#${id}">${name}</a>
+                <a class="trigger build_child link_date" href="#${id}">${builds[id].date}</a>
+                <a class="build_child link_commit" href=https://github.com/${owner}/${repository}/commit/${builds[id].sha}>#${builds[id].sha.substr(0, 5)}</a>
+            </div>
+        `;
     }
 });
 
@@ -215,14 +214,14 @@ function createBadge(directory, language) {
     var url = "";
 
     if (language === "markdown") {
-        url = "[![Build Status](https://thebusybiscuit.github.io/builds/" + directory + "/badge.svg)](https://thebusybiscuit.github.io/builds/" + directory + ")"
-    }
-    else if (language === "html") {
-        url = "<a href=\"https://thebusybiscuit.github.io/builds/" + directory + "\"><img src=\"https://thebusybiscuit.github.io/builds/" + directory + "/badge.svg\" alt=\"Build Status\"/></a>";
+        url = `[![Build Status](https://thebusybiscuit.github.io/builds/${directory}/badge.svg)](https://thebusybiscuit.github.io/builds/${directory})`;
+    } else if (language === "html") {
+        url = `<a href="https://thebusybiscuit.github.io/builds/${directory}"><img src="https://thebusybiscuit.github.io/builds/${directory}/badge.svg" alt="Build Status"/></a>`;
     }
 
     $("#badge_" + language).attr("value", url);
-    $("#copy_" + language).click(function() {
+
+    $("#copy_" + language).click(() => {
         $("#badge_" + language).select();
         document.execCommand("Copy");
     });
--- src/index.js
+++ src/index.js
@@ -20,8 +20,8 @@ $(function() {
 function addProfile(user, repos) {
     $("#repos").append(
         `<div class="box" style="width: 65%; margin: 2vh auto;">
-            <a style="padding-top: 1vh;" class="link_build link_repo" href="https://github.com/${user}">
-                <img alt="profil" src="https://cdnjs.cloudflare.com/ajax/libs/octicons/8.5.0/svg/person.svg" class="profile_icon">
+            <a style="padding-top: 1vh;" class="link_repo" href="https://github.com/${user}">
+                <img alt="profile" src="https://cdnjs.cloudflare.com/ajax/libs/octicons/8.5.0/svg/person.svg" class="profile_icon">
                 <span>${user}</span>
             </a>
             <table id="projects_${user}" class="info_table"></table>
@@ -51,28 +51,32 @@ function addProfile(user, repos) {
         addRepository(table, directory, user, repository, branch, projectType);
     }
 
-    // Alphabetical sorting
-    let items = table.children("tr").sort((a, b) => {
-        let repo1 = $(a).attr("project").toUpperCase();
-        let repo2 = $(b).attr("project").toUpperCase();
-        return (repo1 > repo2) ? 1 : -1;
-    });
-
-    $(table).append(items);
+    // Alphabetical sorting (abandoned projects to the bottom)
+    $(table).append(table.children("tr").sort((a, b) => sortProjects($(a), $(b))));
 }
 
 function addRepository(table, directory, owner, repo, branch, projectType) {
     table.append(
         `<tr class="${projectType}" project="${repo}:${branch}">
             <td class="icon">
-                <img class="icon" alt="project page" src="https://cdnjs.cloudflare.com/ajax/libs/octicons/8.5.0/svg/beaker.svg" />
+                <img alt="project page" src="https://cdnjs.cloudflare.com/ajax/libs/octicons/8.5.0/svg/beaker.svg" />
             </td>
             <td class="table_label" style="width: auto;">
-                <a class="link_info" href="${directory}">${repo} (${branch})${projectType == 'abandoned' ? " [abandoned]": ""}</a>
+                <a class="link_info" href="${directory}">${repo} (${branch})${projectType == 'abandoned' ? " [abandoned]" : ""}</a>
             </td>
             <td>
                 <img style="float: right;" alt="project badge" src="https://thebusybiscuit.github.io/builds/${directory}/badge.svg" />
             </td>
         </tr>
     `);
+}
+
+function sortProjects(a, b) {
+    if (a.hasClass("abandoned") && !b.hasClass("abandoned")) {
+        return 1;
+    } else if (!a.hasClass("abandoned") && b.hasClass("abandoned")) {
+        return -1;
+    } else {
+        return a.attr("project").toUpperCase() > b.attr("project").toUpperCase() ? 1 : -1;
+    }
 }
