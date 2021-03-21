080fc26c90fbc4080478ad36a467bc0ff62819d6
RobotHanzo <nathanlee302@outlook.com>
2021-03-21 20:51:32 +0800

    New lines fixed

--- test/TestGradle.js
+++ test/TestGradle.js
@@ -26,11 +26,11 @@ describe('Gradle Test', () => {
     it('should create a gradle.properties file with the right version when there isn\'t one', async () => {
       await fs.mkdir(path.resolve(__dirname, '../' + fakeJob.directory + '/files'))
       assert.isFulfilled(gradle.setVersion(fakeJob, '1.1'))
-      assert.equal(await fs.readFile(path.resolve(__dirname, '../' + fakeJob.directory + '/files/gradle.properties'), 'utf8'), 'version=1.1')
+      assert.equal(await fs.readFile(path.resolve(__dirname, '../' + fakeJob.directory + '/files/gradle.properties'), 'utf8'), '\nversion=1.1')
     })
     it('should edit the version of a gradle.properties file', async () => {
       assert.isFulfilled(gradle.setVersion(fakeJob, '1.2'))
-      assert.equal(await fs.readFile(path.resolve(__dirname, '../' + fakeJob.directory + '/files/gradle.properties'), 'utf8'), 'version=1.2')
+      assert.equal(await fs.readFile(path.resolve(__dirname, '../' + fakeJob.directory + '/files/gradle.properties'), 'utf8'), '\nversion=1.2')
     })
   })
 
