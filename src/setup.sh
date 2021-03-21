d7bcd662206f6560e48b4e65d8c10dc2293351b3
RobotHanzo <nathanlee302@outlook.com>
2021-03-21 14:47:37 +0800

    Changed to repo

--- src/setup.sh
+++ src/setup.sh
@@ -1,4 +1,5 @@
 git config user.name "TheBusyBot"
 git config user.email ${LOGIN_EMAIL}
 
-git remote set-url origin https://${ACCESS_TOKEN}@github.com/TheBusyBiscuit/builds.git
+#git remote set-url origin https://${ACCESS_TOKEN}@github.com/TheBusyBiscuit/builds.git
+git remote set-url origin https://${ACCESS_TOKEN}@github.com/RobotHanzo/TheBusyBiscuit-builds.git
